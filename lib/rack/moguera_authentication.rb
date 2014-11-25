27a6b52fa92c0cfc966f67226641a50551895672
hiro-su <h.sugipon@gmail.com>
2014-11-25 13:49:04 +0900

    add rack middleware

---
+++ lib/rack/moguera_authentication.rb
@@ -0,0 +1,44 @@
+require 'moguera/authentication'
+
+module Rack
+  # A Rack middleware inserts a Moguera Authentication.
+  #
+  # @example config.ru
+  #   # Add Rack::MogueraAuthentication somewhere in your rack stack
+  #   use Rack::MogueraAuthentication do |request_access_key|
+  #
+  #     # Search a secret_access_key by request_access_key
+  #     key_to_secret(request_access_key)
+  #   end
+  class MogueraAuthentication
+    def initialize(app, &block)
+      @app = app
+      @secret_block = block
+    end
+
+    def call(env)
+      request_token = Moguera::Authentication.new(env['HTTP_AUTHORIZATION'])
+      request_token.authenticate! do |key|
+        secret = @secret_block.call(key)
+        params = build_parameter(key, secret, env)
+
+        Moguera::Authentication::Request.new(params)
+      end
+
+      @app.call(env)
+    end
+
+    private
+
+    def build_parameter(key, secret, env)
+      {
+          access_key: key,
+          secret_access_key: secret,
+          request_path: env['REQUEST_PATH'],
+          request_method: env['REQUEST_METHOD'],
+          content_type: env['CONTENT_TYPE'],
+          http_date: env['HTTP_DATE']
+      }
+    end
+  end
+end
